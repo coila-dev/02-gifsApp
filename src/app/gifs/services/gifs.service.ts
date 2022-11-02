0c540cf6a45e25737ccabe153d310c6f1ce9995a
coila-dev <carlos162188@gmail.com>
2022-11-02 12:32:09 -0500

    localstorage y cargado de historial y resultado automaticamente

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -15,7 +15,10 @@ export class GifsService {
   // get resultados(){
   //   return [...this._resultados];
   // }
-  constructor(private http:HttpClient){};
+  constructor(private http:HttpClient){
+    this._historial=JSON.parse(localStorage.getItem('historial')!)||[];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!)||[];
+  };
   buscarGifs(query:string){
     // if (this._historial.indexOf(query)!=-1) {
     //   return;
@@ -24,11 +27,16 @@ export class GifsService {
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       console.log(this._historial);  
+      // localStorage.setItem('historial',query);
+      localStorage.setItem('historial',JSON.stringify(this._historial));
+      
     }
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
     .subscribe((resp) =>{
       console.log(resp.data);
       this.resultados = resp.data;
+      localStorage.setItem('resultados',JSON.stringify(this.resultados));
     });
+    
   }
 }
